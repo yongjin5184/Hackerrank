227866bfffa6e1d6eba523753d86c4278b247545
Kimyongjin <yongjin5184@gmail.com>
2017-08-31 18:56:57 +0900

    Day 17: More Exceptions

---
+++ src/30DaysOfCode/Day17.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.lang.*;
+/**
+ * Created by yong on 2017. 8. 31..
+ * Day 17: More Exceptions
+ */
+public class Day17 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
+}
+
+class Calculator{
+    public int power(int n, int p) throws Exception{
+
+        if (n < 0 || p < 0){
+            throw new Exception("n and p should be non-negative");
+
+        }else {
+            int result = (int) Math.pow(n, p);
+            return result;
+        }
+    }
+}
