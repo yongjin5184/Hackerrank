8b5f051d807ce3cacb2234562bc094e25c7011fa
김용진 <[yongjin@st-company.com]>
2017-09-04 18:45:44 +0900

    Day 21: Generics

---
+++ src/30DaysOfCode/Day21.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * Created by yongjin on 2017-09-04.
+ * Day 21: Generics
+ */
+class Printer <T> {
+    public void printArray(T[] arr){
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
+
+public class Day21 {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    }
+}
